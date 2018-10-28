ae3430d26553aa0a780f597855374fe260835bb6
neelarka <tinku.supakar@gmail.com>
2018-10-28 15:30:20 -0500

    changed the app.js file code

--- static/js/app.js
+++ static/js/app.js
@@ -34,51 +34,49 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  d3.json(`/samples/${sample}`).then(success1)
+d3.json(`/samples/${sample}`).then((data) => {
 
-  // @TODO: Build a Bubble Chart using the sample data
-
-function success1(data){
-  var trace1 = {
-    x: data["otu_ids"],
-    y: data["sample_values"],
-    text: data["otu_labels"],
+  var bubbleData = [{
+    x: data.otu_ids,
+    y: data.sample_values,
+    text: data.otu_labels,
     mode: 'markers',
     marker: {
-      size: data["sample_values"],
-      color: data["otu_ids"],
+    size: data.sample_values,
+    color: "Earth",
     }
-  };
-
-  var data = [trace1];
-
+  }];
+  
   var layout = {
     title: 'Germs in the sample',
+    margin: {t :0},
+    hovermode: "closest",
     showlegend: false
   };
 
-  Plotly.newPlot("bubble", data, layout);
+  Plotly.plot("bubble", bubbleData, layout);
+
+  var pieData = [{
+    values:data["sample_values"].slice(0,10),
+    labels:data["otu_ids"].slice(0,10),
+    hovertext: data["otu_labels"].slice(0, 10),
+    hoverinfo: 'hovertext',
+    type: "pie"
+  }]
+
+  var layout2 = {"title" : "Top 10 Operational Taxonomic Units <br> (OTU) found in this sample"}
+    
+  Plotly.plot("pie", pieData, layout2)
+
+})
+
 }
+  // @TODO: Build a Bubble Chart using the sample data
+
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-
-    d3.json(`/samples/${sample}`).then(success2)
-    function success2(data){
-      var plot_data = [{
-        values:data["sample_values"].slice(0,10),
-        labels:data["otu_ids"].slice(0,10),
-        hovertext: data["otu_labels"].slice(0, 10),
-        hoverinfo: 'hovertext',
-        type: "pie"
-      }]
-      var layout = {"title" : "Top 10 Operational Taxonomic Units <br> (OTU) found in this sample"}
-      Plotly.newPlot("pie", plot_data, layout)
-    }
-
-
-}
 
 function init() {
   // Grab a reference to the dropdown select element
