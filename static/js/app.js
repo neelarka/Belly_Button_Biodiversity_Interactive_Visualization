42632665ec2d541383c6c998aa5294004835cab3
Tinku Supakar <38635204+neelarka@users.noreply.github.com>
2018-10-28 22:08:14 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -31,6 +31,10 @@ function buildMetadata(sample) {
 
 }
 
+  // @TODO: Build a Bubble Chart using the sample data
+  // @TODO: Build a Pie Chart
+  // HINT: You will need to use slice() to grab the top 10 sample_values,
+  // otu_ids, and labels (10 each).
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
@@ -71,12 +75,7 @@ d3.json(`/samples/${sample}`).then((data) => {
 })
 
 }
-  // @TODO: Build a Bubble Chart using the sample data
 
-
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
 
 function init() {
   // Grab a reference to the dropdown select element
